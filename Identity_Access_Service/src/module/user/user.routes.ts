51c72e4152fb417934941222f6acf829999f3071
Istian <markchristianforlaje@gmail.com>
2026-06-11 13:47:01 +0800

    feat(git_user): Add listing functionality and get by Id

--- Identity_Access_Service/src/module/user/user.routes.ts
+++ Identity_Access_Service/src/module/user/user.routes.ts
@@ -1,10 +1,17 @@
-import {registerUserHandler, 
-    updateUserInfoHandler} from "./user.controller";
+import {
+    registerUserHandler, 
+    updateUserInfoHandler,
+    getUserByIdHandler,
+    listUsersHandler
+} from "./user.controller";
 import { Router } from "express";
 
 const router = Router();
 
 router.post('/', registerUserHandler);
 router.patch('/:id', updateUserInfoHandler);
+router.get('/:id', getUserByIdHandler);
+router.get('/', listUsersHandler);
+
 
 export default router;
